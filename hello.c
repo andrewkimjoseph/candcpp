1f56f46eae8caca3cb3d8295bb8098ab10c0714a
Andrew Kim <andrewkimjoseph@gmail.com>
2022-03-29 09:17:42 +0300

    C Practice

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+
+    int a = 10;
+
+    int b = 2;
+
+    int sum;
+
+    sum = a + b;
+    
+    printf("The Sum of %d and %d is %d", a, b, sum);
+
+    return 0;
+}
